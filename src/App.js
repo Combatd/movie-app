4a3ff1254223dc38e4b958b1524ecbfb97d7dc5a
Michael <msiller455@gmail.com>
2019-12-02 13:59:57 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  async componentDidMount() {
+    const message = await fetch('/api/v1/hello')
+    const messageJson = await message.json()
+    console.log(messageJson)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        <h1>Hey dude!</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
