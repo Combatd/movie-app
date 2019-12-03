7306527e79a18eaa6c889e7f637db31ecd03055c
Michael <msiller455@gmail.com>
2019-12-02 21:49:32 -0800

    fixes gitignore

---
+++ build/precache-manifest.eb17f133984f50f909525d15475f569b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86aa5b1fcfa4cca46e7fea3292bc030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "656e8684777b9d672290",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "af04b806cbb71996bff8",
+    "url": "/static/js/2.30e96410.chunk.js"
+  },
+  {
+    "revision": "656e8684777b9d672290",
+    "url": "/static/js/main.20b56665.chunk.js"
+  },
+  {
+    "revision": "7513028020f0ce66a1ac",
+    "url": "/static/js/runtime-main.05f0f1c8.js"
+  }
+]);
